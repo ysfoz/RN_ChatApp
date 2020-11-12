b5ec3e85d1b0adb63398eeaba5f44e3516009a7e
yusuf <ysfozturk3@gmail.com>
2020-11-12 11:02:11 +0100

    created

---
+++ ios/Pods/Target Support Files/Pods-RN_ChatApp-RN_ChatAppTests/Pods-RN_ChatApp-RN_ChatAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RN_ChatApp_RN_ChatAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_RN_ChatApp_RN_ChatAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-RN_ChatApp/Pods-RN_ChatApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RN_ChatApp : NSObject
+@end
+@implementation PodsDummy_Pods_RN_ChatApp
+@end
