f0985911a7a66ec5634d0966605bd412ad0c021b
yusuf <ysfozturk3@gmail.com>
2020-11-13 22:54:08 +0100

    connected sending Text with Firebase

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,17 +1,35 @@
 import React,{useState} from 'react';
 import {SafeAreaView, View, Text, FlatList} from 'react-native';
 import auth from '@react-native-firebase/auth'
-
+import database from '@react-native-firebase/database'
 
 import {timelinePage} from './styles';
 import {PostItem, PostInput, Header, TopicSelectModal} from '../components';
 
+const user = auth().currentUser; // firebase kullanildiginda usestorage yada redux a
+// gerek kalmasdan , bu sekilde verilere ulasmamk mumkun.
 const Timeline = () => {
-  const user = auth().currentUser; // firebase kullanildiginda usestorage yada redux a
-  // gerek kalmasdan , bu sekilde verilere ulasmamk mumkun.
 
   const [topicModalFlag,setTopicModalFlag] = useState(true)
   const [selectedTopic,setSelectedTopic] = useState(null)
+
+  const selectingTopic =(value)=>{
+    setSelectedTopic(value);
+    setTopicModalFlag(false)
+
+    database()
+    .ref()
+    // . on yerine .once yazilsa guncelleme yapmaz, yani axios gibi veriyi bir defa alir, .on olunca her degisikligi okur.
+    .on('value',(snapshot)=>{
+      console.log(snapshot.val())
+    })
+
+  }
+  
+  const sendingPost = (value)=>{
+    console.log(value)
+  }
+
   return (
     <SafeAreaView style={timelinePage.container}>
       <View style={timelinePage.container}>
@@ -25,15 +43,13 @@ const Timeline = () => {
         renderItem ={()=> null}
         />
         <PostInput
-        onSendPost ={(value) =>console.log(value)}
+        onSendPost ={sendingPost}
         />
         <TopicSelectModal
         visibility = {topicModalFlag}
-        onClose = {()=> selectedTopic ? setTopicModalFlag(false):null}
-        onTopicSelect ={(value)=>{
-          setSelectedTopic(value);
-          setTopicModalFlag(false)
-        } }
+        onClose = {()=> selectedTopic && setTopicModalFlag(false)}
+        onTopicSelect ={selectingTopic}
+       
         />
       </View>
     </SafeAreaView>
